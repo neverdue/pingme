669fd092327aab1cb82925d4eff3513f9d53685b
Mayank Pandey <mp.pandey86@gmail.com>
2020-06-13 01:16:10 +0530

    Initial commit with gitignore and a basic registration page

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+from forms import *
+
+app = Flask(__name__)
+app.secret_key = "replace this"
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great Success"
+    return render_template("index.html", form=reg_form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
