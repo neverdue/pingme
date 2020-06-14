25cf049b03a2143866373476084cabd159fab22d
Mayank Pandey <mp.pandey86@gmail.com>
2020-06-14 20:10:12 +0530

    Added styles and formatting

--- static/scripts/chat_page.js
+++ static/scripts/chat_page.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Make enter/return key send message
-  let msg = document.querySelector('#user_message');
-  msg.addEventListener('keyup', event => {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      document.querySelector('#send-message').click();
-    }
-  })
-})
+
+    // Make sidebar collapse on click
+    document.querySelector('#show-sidebar-button').onclick = () => {
+        document.querySelector('#sidebar').classList.toggle('view-sidebar');
+    };
+
+    // Make 'enter' key submit message
+    let msg = document.getElementById("user_message");
+    msg.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("send_message").click();
+        }
+    });
+});
